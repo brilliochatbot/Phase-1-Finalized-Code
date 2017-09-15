0bd1aa6213dda5c4fe88aa449e123967eeed80f3
Jinu Kuruvilla <jinu.kuruvilla@BRILLIO.COM>
2017-09-15 12:35:21 +0530

    freezed code

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var request = require('request');
 var apiai = require('apiai');
 var app = apiai("246c44caf97540f6a617d1f021fa8d4a");
 
-//const AIMLInterpreter = require('AIMLInterpreter');
+
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
@@ -20,33 +20,6 @@ var connector = new builder.ChatConnector({
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-// var bot = new builder.UniversalBot(connector);
-// const aimlInterpreter = new AIMLInterpreter({ name: 'John', age: '25' });
-// aimlInterpreter.loadAIMLFilesIntoArray(['./aiml-en-us-foundation-alice.v1-6']);
- 
-// const aimlPromise = function (question) {
-//     return new Promise(function (resolve, reject) {
-//         aimlInterpreter.findAnswerInLoadedAIMLFiles(question, function (answer, wildCardArray, input) {
-//             return resolve(answer);
-// //should also handle reject!!! this is demo code only :)
-//         });
-//     })
-// };
-
-
-// bot.dialog('/', [
-//     function (session) {
-//         builder.Prompts.text(session, 'Hi! Ask me something...');
-//     },
-//     function (session, results) {
-//         aimlPromise(results.response).then(res ==gt; session.send(`${res}`));
-//     }
-// ]);
-
-
-
-
-// Add global LUIS recognizer to bot
 
 var bot = new builder.UniversalBot(connector);
 
@@ -73,15 +46,13 @@ var e2=0;
 
 bot.dialog('/', intents);
 
- // intents.onDefault(prioritizer());
+
 
 
     intents.matches('MainFlow', [
   
     function (session, args) {
-    // var intent = args.intent;
-     //    var ent_1 = builder.EntityRecognizer.findEntity(intent.entities, 'ServiceType');
-       console.log(args.entities);
+
            
             if(builder.EntityRecognizer.findEntity(args.entities, 'Service')){
            
@@ -158,84 +129,80 @@ f1=0;
  
   if(a1==0){
 
-//  if(a2==0){
-//  a2=a2+1;
+  if(a2==0){
+  a2=a2+1;
   session.send( "I can help you to book a service quickly provided I get few necessary information.");
   session.send( "Let me know your car number!"); 
- // }
-//  else if(a2>1){
-//  var num = Math.floor(Math.random() * 3) + 1;
-//            if(num==1){session.send("It seems I did not get what I wanted from you."); session.send("Before I forget again, can you repeat your car number?");}
-//  if(num==2){session.send("Your car number should be of the form xxxx yyy where x is an alphabet and y is a number."); session.send("Could repeat your car number in that format?");}
-//  if(num==3){session.send("Just a second.."); session.send("Could repeat your car number again? I will make a note of that before I forget!");} 
-//  }
+  }
+  else if(a2>1){
+  var num = Math.floor(Math.random() * 3) + 1;
+            if(num==1){session.send("It seems I did not get what I wanted from you."); session.send("Before I forget again, can you repeat your car number?");}
+  if(num==2){session.send("Your car number should be of the form xxxx yyy where x is an alphabet and y is a number."); session.send("Could repeat your car number in that format?");}
+  if(num==3){session.send("Just a second.."); session.send("Could repeat your car number again? I will make a note of that before I forget!");}
+  }
  
   }
 
 else if(b1==0){
 
-//  if(b2==0){
-//  b2=b2+1;
+  if(b2==0){
+  b2=b2+1;
   session.send( "Just a second!");
   session.send( "Hey John! Hope you are riding your Lexus RC F well. Good to see you back after six months.");
   session.send( "Let me know whether you wish to go for a routine service or an auxiliary service this time?");
-//  }
-//  else if(b2>1){
-//  var num = 1;
-//           if(num==1){session.send("I have a strange habit not talking about any other thing before I complete my current task.");session.send("Can you tell me whether you prefer an auxiliary service or a routine service?"); session.send("Before I forget again, can you repeat your car number?");}
-//  //if(num==2){session.send("Your car number should be of the form xxxx yyy where x is an alphabet and y is a number."); session.send("Could repeat your car number in that format?");}
-//  //if(num==3){session.send("Just a second.."); session.send("Could repeat your car number again? I will make a note of that before I forget!");} 
-//  }
-  
+  }
+  else if(b2>1){
+  var num = 1;
+           if(num==1){session.send("I have a strange habit not talking about any other thing before I complete my current task.");session.send("Can you tell me whether you prefer an auxiliary service or a routine service?"); session.send("Before I forget again, can you repeat your car number?");}
+ 
+  }
+ 
   }
 
 else if(c1==0){
-//  if(c2==0){
-//  c2=c2+1;
+  if(c2==0){
+  c2=c2+1;
   session.send( "Alright!");
   session.send( "Let me know the date and time you are comfortable with.."); 
-//  }
-//  else if(c2>1){
-//  var num = 1;
-//            if(num==1){session.send("Date and time is very important for booking this service."); session.send("Before we move any further, give me the date and time.");}
-//  //if(num==2){session.send("Your car number should be of the form xxxx yyy where x is an alphabet and y is a number."); session.send("Could repeat your car number in that format?");}
-//  //if(num==3){session.send("Just a second.."); session.send("Could repeat your car number again? I will make a note of that before I forget!");} 
-//  }
+  }
+  else if(c2>1){
+  var num = 1;
+            if(num==1){session.send("Date and time is very important for booking this service."); session.send("Before we move any further, give me the date and time.");}
+
+  }
  
   }
 
 else if(d1==0){
-  
-//  if(d2==0){
-//  d2=d2+1;
+ 
+  if(d2==0){
+  d2=d2+1;
   session.send( "Fine! Noted it.");
   session.send( "Generally people go for oil change, battery check-up, general servicing during a routine service.");
   session.send("What are your preferences?");
-//  }
-//  else if(d2>1){
-//  var num = 1;
-//           if(num==1){session.send("Hey its very important for me to know the preferences.");session.send("Kindly bare to repeat the service preferences again.");}
-//  //if(num==2){session.send("Your car number should be of the form xxxx yyy where x is an alphabet and y is a number."); session.send("Could repeat your car number in that format?");}
-//  //if(num==3){session.send("Just a second.."); session.send("Could repeat your car number again? I will make a note of that before I forget!");} 
-//  }
-  
+  }
+  else if(d2>1){
+  var num = 1;
+           if(num==1){session.send("Hey its very important for me to know the preferences.");session.send("Kindly bare to repeat the service preferences again.");}
+ 
+ 
   }
   
     else if(e1==0){
-//    if(e2==0){
-//  e2=e2+1;
+    if(e2==0){
+  e2=e2+1;
   session.send( "Okay!");
   session.send( "While we service your car, do you prefer a Loaner car or Shuttle sevrice alternative?"); 
-//  }
-//  else if(e2>1){
-//  var num = 1;
-//            if(num==1){session.send("Hey let me know whether you prefer a Loaner car or a Shuttle service..."); }
-//  //if(num==2){session.send("Your car number should be of the form xxxx yyy where x is an alphabet and y is a number."); session.send("Could repeat your car number in that format?");}
-//  //if(num==3){session.send("Just a second.."); session.send("Could repeat your car number again? I will make a note of that before I forget!");} 
-//  }
+  }
+  else if(e2>1){
+  var num = 1;
+            if(num==1){session.send("Hey let me know whether you prefer a Loaner car or a Shuttle service..."); }
+ 
+  }
  
   }
       }
+ 
     return;
 
   }
@@ -243,6 +210,7 @@ else if(d1==0){
   
 function apiCall(session, args){
 console.log("Inside")
+ 
 var a = session.message.text;
 var request = app.textRequest(a, {
     sessionId: '1234567891'
@@ -264,5 +232,5 @@ return;
 
 
 
-}
 
+}
